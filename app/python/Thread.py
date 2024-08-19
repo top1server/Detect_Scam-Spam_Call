24471a1165274ac4f851d5a98ebab3aaf4eb414c
top1server <mightdung7105@gmail.com>
2024-08-20 02:56:01 +0700

    Muti Thread and Test

---
+++ app/python/Thread.py
@@ -0,0 +1,24 @@
+import sys
+import os
+sys.dont_write_bytecode = True
+sys.path.append(os.path.abspath('/Detect_scam-spam_call'))
+from app.python.Record import Record
+from app.python.Convert import Convert
+import threading
+
+class Thread:
+    @staticmethod
+    def main():
+        record_seconds = None
+        
+        recorder = Record(record_seconds=record_seconds)
+        converter = Convert(queue=recorder.queue, rate=recorder.rate, width=recorder.audio.get_sample_size(recorder.format))
+        record_thread = threading.Thread(target=recorder.startRecord)
+        stt_thread = threading.Thread(target=converter.convert)
+
+        record_thread.start()
+        stt_thread.start()
+        record_thread.join()
+        stt_thread.join()
+        recorder.terminate()
+    
---
+++ test/Thread.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.dont_write_bytecode = True
+sys.path.append(os.path.abspath('/Detect_scam-spam_call'))
+from app.python.Thread import Thread
+
+if __name__ == "__main__":
+    Thread.main()
